5915f7abf4de29a747caee83daac9d224e5c8516
Yu-peng Fu <yfu@z3r12p1.42lyon.fr>
2021-07-15 17:06:32 +0200

    not finished yet

--- microshell/microshell.c
+++ microshell/microshell.c
@@ -1,267 +1,101 @@
+// start from 15h
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <signal.h>
 #include <string.h>
 
-#define SIDE_OUT	0
-#define SIDE_IN		1
-
-#define STDIN		0
-#define STDOUT		1
-#define STDERR		2
-
-#define TYPE_END	0
-#define TYPE_PIPE	1
-#define TYPE_BREAK	2
-
-#ifdef TEST_SH
-# define TEST		1
-#else
-# define TEST		0
-#endif
-
-typedef struct	s_list
+typedef struct s_list
 {
-	char			**args;
-	int				length;
-	int				type;
-	int				pipes[2];
-	struct s_list	*previous;
-	struct s_list	*next;
-}				t_list;
+	char *str; // no need to free
+	struct s_list *next;
+	struct s_list *prev;
+}t_list;
 
-int ft_strlen(char const *str)
+t_list	*command = NULL;
+char	**env = NULL;
+int		pipe_exist = 0;
+int		fd[2][2] = {{0,1},{0,1}};
+
+void command_push_back(char *str)
 {
-	int	i;
+	t_list *_new = malloc(1 * sizeof(t_list));
+	_new->next = NULL;
+	_new->str = str;
+	_new->prev = NULL;
+	if (command == NULL)
+	{
+		command = _new;
+	}
+	else
+	{
+		t_list *temp = command;
+		while (temp->next) temp = temp->next;
+		temp->next = _new;
+		_new->prev = temp;
+	}
+}
 
-	i = 0;
+void command_pop_front()
+{
+	if (command)
+	{
+		void *ptr = command;
+		command = command->next;
+		free(ptr);
+		if (command)
+			command->prev = NULL;
+	}
+}
+
+void command_clear()
+{
+	while (command)
+	{
+		command_pop_front();
+	}
+}
+
+int ft_strlen(char *str)
+{
+	if (!str)
+		return (0);
+	int i = 0;
 	while (str[i])
-		i++;
+		++i;
 	return (i);
 }
 
-int show_error(char const *str)
+void ft_putstr_fd(char *str, int fd)
 {
-	if (str)
-		write(STDERR, str, ft_strlen(str));
-	return (EXIT_FAILURE);
+	write(fd, str, ft_strlen(str));
 }
 
-int exit_fatal(void)
+void run_command() // in parent process : free command, change fd[2][2] back, pipe_exist = 0
 {
-	show_error("error: fatal\n");
-	exit(EXIT_FAILURE);
-	return (EXIT_FAILURE);
+
 }
 
-void *exit_fatal_ptr(void)
+int main(int ac, char **av, char **_env)
 {
-	exit_fatal();
-	exit(EXIT_FAILURE);
-	return (NULL);
-}
-
-char *ft_strdup(char const *str)
-{
-	char	*copy;
-	int		i;
-
-	if (!(copy = (char*)malloc(sizeof(*copy) * (ft_strlen(str) + 1))))
-		return (exit_fatal_ptr());
-	i = 0;
-	while (str[i])
+	env = _env;
+	for (int i = 0 ; i < ac ; ++i)
 	{
-		copy[i] = str[i];
-		i++;
-	}
-	copy[i] = 0;
-	return (copy);
-}
-
-int add_arg(t_list *cmd, char *arg)
-{
-	char	**tmp;
-	int		i;
-
-	i = 0;
-	tmp = NULL;
-	if (!(tmp = (char**)malloc(sizeof(*tmp) * (cmd->length + 2))))
-		return (exit_fatal());
-	while (i < cmd->length)
-	{
-		tmp[i] = cmd->args[i];
-		i++;
-	}
-	if (cmd->length > 0)
-		free(cmd->args);
-	cmd->args = tmp;
-	cmd->args[i++] = ft_strdup(arg);
-	cmd->args[i] = 0;
-	cmd->length++;
-	return (EXIT_SUCCESS);
-}
-
-int list_push(t_list **list, char *arg)
-{
-	t_list	*new;
-
-	if (!(new = (t_list*)malloc(sizeof(*new))))
-		return (exit_fatal());
-	new->args = NULL;
-	new->length = 0;
-	new->type = TYPE_END;
-	new->previous = NULL;
-	new->next = NULL;
-	if (*list)
-	{
-		(*list)->next = new;
-		new->previous = *list;
-	}
-	*list = new;
-	return (add_arg(new, arg));
-}
-
-int list_rewind(t_list **list)
-{
-	while (*list && (*list)->previous)
-		*list = (*list)->previous;
-	return (EXIT_SUCCESS);
-}
-
-int list_clear(t_list **cmds)
-{
-	t_list	*tmp;
-	int		i;
-
-	list_rewind(cmds);
-	while (*cmds)
-	{
-		tmp = (*cmds)->next;
-		i = 0;
-		while (i < (*cmds)->length)
-			free((*cmds)->args[i++]);
-		free((*cmds)->args);
-		free(*cmds);
-		*cmds = tmp;
-	}
-	*cmds = NULL;
-	return (EXIT_SUCCESS);
-}
-
-int parse_arg(t_list **cmds, char *arg)
-{
-	int	is_break;
-
-	is_break = (strcmp(";", arg) == 0);
-	if (is_break && !*cmds)
-		return (EXIT_SUCCESS);
-	else if (!is_break && (!*cmds || (*cmds)->type > TYPE_END))
-		return (list_push(cmds, arg));
-	else if (strcmp("|", arg) == 0)
-		(*cmds)->type = TYPE_PIPE;
-	else if (is_break)
-		(*cmds)->type = TYPE_BREAK;
-	else
-		return (add_arg(*cmds, arg));
-	return (EXIT_SUCCESS);
-}
-
-int exec_cmd(t_list *cmd, char **env)
-{
-	pid_t	pid;
-	int		ret;
-	int		status;
-	int		pipe_open;
-
-	ret = EXIT_FAILURE;
-	pipe_open = 0;
-	if (cmd->type == TYPE_PIPE || (cmd->previous && cmd->previous->type == TYPE_PIPE))
-	{
-		pipe_open = 1;
-		if (pipe(cmd->pipes))
-			return (exit_fatal());
-	}
-	pid = fork();
-	if (pid < 0)
-		return (exit_fatal());
-	else if (pid == 0)
-	{
-		if (cmd->type == TYPE_PIPE
-			&& dup2(cmd->pipes[SIDE_IN], STDOUT) < 0)
-			return (exit_fatal());
-		if (cmd->previous && cmd->previous->type == TYPE_PIPE
-			&& dup2(cmd->previous->pipes[SIDE_OUT], STDIN) < 0)
-			return (exit_fatal());
-		if ((ret = execve(cmd->args[0], cmd->args, env)) < 0)
+		if (strcmp("|", av[i]) == 0)
 		{
-			show_error("error: cannot execute ");
-			show_error(cmd->args[0]);
-			show_error("\n");
-		}
-		exit(ret);
-	}
-	else
-	{
-		waitpid(pid, &status, 0);
-		if (pipe_open)
-		{
-			close(cmd->pipes[SIDE_IN]);
-			if (!cmd->next || cmd->type == TYPE_BREAK)
-				close(cmd->pipes[SIDE_OUT]);
-		}
-		if (cmd->previous && cmd->previous->type == TYPE_PIPE)
-			close(cmd->previous->pipes[SIDE_OUT]);
-		if (WIFEXITED(status))
-			ret = WEXITSTATUS(status);
-	}
-	return (ret);
-}
-
-int exec_cmds(t_list **cmds, char **env)
-{
-	t_list	*crt;
-	int		ret;
-
-	ret = EXIT_SUCCESS;
-	list_rewind(cmds);
-	while (*cmds)
-	{
-		crt = *cmds;
-		if (strcmp("cd", crt->args[0]) == 0)
-		{
-			ret = EXIT_SUCCESS;
-			if (crt->length < 2)
-				ret = show_error("error: cd: bad arguments\n");
-			else if (chdir(crt->args[1]))
+			if (!pipe_exist)
 			{
-				ret = show_error("error: cd: cannot change directory to ");
-				show_error(crt->args[1]);
-				show_error("\n");
+
+			}
+			else
+			{
+
 			}
 		}
+		else if (strcmp(";", av[i]) == 0)
+			run_command();
 		else
-			ret = exec_cmd(crt, env);
-		if (!(*cmds)->next)
-			break ;
-		*cmds = (*cmds)->next;
+			command_push_back(av[i]);
 	}
-	return (ret);
-}
-
-int main(int argc, char **argv, char **env)
-{
-	t_list	*cmds;
-	int		i;
-	int		ret;
-
-	ret = EXIT_SUCCESS;
-	cmds = NULL;
-	i = 1;
-	while (i < argc)
-		parse_arg(&cmds, argv[i++]);
-	if (cmds)
-		ret = exec_cmds(&cmds, env);
-	list_clear(&cmds);
-	if (TEST)
-		while (1);
-	return (ret);
+	run_command();
 }
