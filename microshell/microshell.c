0ed3643210369ece4fa55c1f2abbb0b3cfd401d0
LeoFu9487 <leofu890806@gmail.com>
2021-07-16 01:24:39 +0200

    new one

--- microshell/microshell.c
+++ microshell/microshell.c
@@ -1,101 +1,201 @@
-// start from 15h
-#include <stdlib.h>
+//start from 00:26
 #include <unistd.h>
+#include <stdlib.h>
 #include <sys/wait.h>
-#include <signal.h>
 #include <string.h>
 
-typedef struct s_list
-{
-	char *str; // no need to free
-	struct s_list *next;
-	struct s_list *prev;
-}t_list;
+char **env;
 
-t_list	*command = NULL;
-char	**env = NULL;
-int		pipe_exist = 0;
-int		fd[2][2] = {{0,1},{0,1}};
-
-void command_push_back(char *str)
+int	ft_strlen(char *str)
 {
-	t_list *_new = malloc(1 * sizeof(t_list));
-	_new->next = NULL;
-	_new->str = str;
-	_new->prev = NULL;
-	if (command == NULL)
-	{
-		command = _new;
-	}
-	else
-	{
-		t_list *temp = command;
-		while (temp->next) temp = temp->next;
-		temp->next = _new;
-		_new->prev = temp;
-	}
-}
-
-void command_pop_front()
-{
-	if (command)
-	{
-		void *ptr = command;
-		command = command->next;
-		free(ptr);
-		if (command)
-			command->prev = NULL;
-	}
-}
-
-void command_clear()
-{
-	while (command)
-	{
-		command_pop_front();
-	}
-}
-
-int ft_strlen(char *str)
-{
-	if (!str)
-		return (0);
+	if (!str) return (0);
 	int i = 0;
-	while (str[i])
-		++i;
+	while(str[i]) ++i;
 	return (i);
 }
 
-void ft_putstr_fd(char *str, int fd)
+void	ft_putstr_fd(char *str, int fd)
 {
 	write(fd, str, ft_strlen(str));
 }
 
-void run_command() // in parent process : free command, change fd[2][2] back, pipe_exist = 0
+typedef struct s_list
 {
+	char *str;
+	struct s_list	*next;
+}t_list;
 
+t_list	*cmd = NULL;
+
+void	cmd_push_back(char *str)
+{
+	t_list *_new = malloc(sizeof(t_list));
+	_new->str = str;
+	_new->next = NULL;
+	if (cmd == NULL)
+	{
+		cmd = _new;
+		return ;
+	}
+	t_list *tmp = cmd;
+	while (tmp->next)
+		tmp = tmp->next;
+	tmp->next = _new;
 }
 
-int main(int ac, char **av, char **_env)
+void	cmd_pop_front()
+{
+	if (cmd)
+	{
+		void *ptr = cmd;
+		cmd = cmd->next;
+		free(ptr);
+	}
+}
+
+void	cmd_clear()
+{
+	while (cmd)
+	{
+		cmd_pop_front();
+	}
+}
+
+void	exit_fatal()
+{
+	if (cmd)
+		cmd_clear();
+	ft_putstr_fd("error: fatal\n", 2);
+	exit(EXIT_FAILURE);
+}
+
+int	fd[2] = {0,1};
+
+void	cd() // remember to free
+{
+	char *str[2];
+	if (!cmd)
+	{
+		ft_putstr_fd("error: cd: bad arguments\n", 2);
+		return ;
+	}
+	str[0] = cmd->str;
+	cmd_pop_front();
+	if (!cmd)
+	{
+		ft_putstr_fd("error: cd: bad arguments\n", 2);
+		return ;
+	}
+	str[1] = cmd->str;
+	cmd_pop_front();
+	if (cmd)
+	{
+		ft_putstr_fd("error: cd: bad arguments\n", 2);
+		return ;
+	}
+	if (chdir(str[1]) < 0)
+	{
+		ft_putstr_fd("error: cd: cannot change directory to ", 2);
+		ft_putstr_fd(str[1], 2);
+		ft_putstr_fd("\n", 2);
+		return ;
+	}
+}
+
+void	run_child() // remember to free
+{
+	for (int i = 0; i  < 2 ; ++i)
+	{
+		if (fd[i] != i)
+		{
+			dup2(fd[i], i);
+			close(fd[i]);
+		}
+	}
+	int	cnt = 0;
+	t_list	*tmp = cmd;
+	while (tmp)
+	{
+		++cnt;
+		tmp = tmp->next;
+	}
+	char **args = malloc((1 + cnt) * sizeof(char *));
+	args[cnt] = 0;
+	for (int i = 0 ; i < cnt ; ++i)
+	{
+		args[i] = cmd->str;
+		cmd_pop_front();
+	}
+	execve(args[0], args, env);
+	ft_putstr_fd("error: cannot execute ", 2);
+	ft_putstr_fd(args[0], 2);
+	ft_putstr_fd("\n", 2);
+	free(args);
+	exit(EXIT_FAILURE);
+}
+
+void	run_cmd() // remember to free
+{
+	if (strcmp("cd", cmd->str) == 0)
+	{
+		cd();
+		return ;
+	}
+	if (!cmd) return ;
+	pid_t pid = fork();
+	if (pid < 0)
+		exit_fatal();
+	else if (pid) // parent
+	{
+		waitpid(pid, NULL, 0);
+		cmd_clear();
+		if (fd[0] != 0)
+			close(fd[0]);
+		fd[0] = 0;
+		fd[1] = 1;
+	}
+	else // child
+		run_child();
+}
+
+int	main(int ac, char **av, char **_env)
 {
 	env = _env;
-	for (int i = 0 ; i < ac ; ++i)
+	for (int i = 1 ; i < ac ; ++i)
 	{
 		if (strcmp("|", av[i]) == 0)
 		{
-			if (!pipe_exist)
+			int	pipefd[2];
+			if (pipe(pipefd) < 0)
+				exit_fatal();
+			pid_t	pid = fork();
+			if (pid < 0)
+				exit_fatal();
+			else if (pid) // parent
 			{
-
+				close(pipefd[1]);
+				cmd_clear();
+				waitpid(pid, NULL, 0);
+				if (fd[0] != 0)
+					close(fd[0]);
+				fd[0] = pipefd[0];
 			}
-			else
+			else // child
 			{
-
+				close(pipefd[0]);
+				fd[1] = pipefd[1];
+				run_child();
 			}
+			
 		}
 		else if (strcmp(";", av[i]) == 0)
-			run_command();
+		{
+			if (cmd)
+				run_cmd();
+		}
 		else
-			command_push_back(av[i]);
+			cmd_push_back(av[i]);
 	}
-	run_command();
+	if (cmd)
+		run_cmd();
 }
