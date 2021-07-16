ec7da6213f5d1aaec05458bf481bc1ebc5014e41
Yu-peng Fu <yfu@z3r10p1.42lyon.fr>
2021-07-16 14:24:14 +0200

    practice one more time

--- microshell/microshell.c
+++ microshell/microshell.c
@@ -1,17 +1,16 @@
-//start from 00:26
+//start from 13h40
 #include <unistd.h>
 #include <stdlib.h>
 #include <sys/wait.h>
 #include <string.h>
 
-char **env;
-
-int	ft_strlen(char *str)
+char	**env = NULL;
+int		ft_strlen(char *str)
 {
-	if (!str) return (0);
-	int i = 0;
-	while(str[i]) ++i;
-	return (i);
+	if (!str) return 0;
+	int n = 0;
+	while (str[n]) ++n;
+	return n;
 }
 
 void	ft_putstr_fd(char *str, int fd)
@@ -19,38 +18,20 @@ void	ft_putstr_fd(char *str, int fd)
 	write(fd, str, ft_strlen(str));
 }
 
-typedef struct s_list
+typedef struct	s_list
 {
-	char *str;
+	char			*str;
 	struct s_list	*next;
 }t_list;
 
 t_list	*cmd = NULL;
 
-void	cmd_push_back(char *str)
-{
-	t_list *_new = malloc(sizeof(t_list));
-	_new->str = str;
-	_new->next = NULL;
-	if (cmd == NULL)
-	{
-		cmd = _new;
-		return ;
-	}
-	t_list *tmp = cmd;
-	while (tmp->next)
-		tmp = tmp->next;
-	tmp->next = _new;
-}
-
 void	cmd_pop_front()
 {
-	if (cmd)
-	{
-		void *ptr = cmd;
-		cmd = cmd->next;
-		free(ptr);
-	}
+	if (!cmd) return ;
+	void	*ptr = cmd;
+	cmd = cmd->next;
+	free(ptr);
 }
 
 void	cmd_clear()
@@ -61,50 +42,59 @@ void	cmd_clear()
 	}
 }
 
-void	exit_fatal()
+void	exit_fatal() // remember to free
 {
-	if (cmd)
-		cmd_clear();
+	if (cmd) cmd_clear();
 	ft_putstr_fd("error: fatal\n", 2);
 	exit(EXIT_FAILURE);
 }
 
-int	fd[2] = {0,1};
-
-void	cd() // remember to free
+void	cmd_push_back(char *str)
 {
-	char *str[2];
-	if (!cmd)
+	t_list	*new = malloc(sizeof(t_list));
+	if (!new)
+		exit_fatal();
+	new->str = str;
+	new->next = NULL;
+	if (!cmd) cmd = new;
+	else
 	{
-		ft_putstr_fd("error: cd: bad arguments\n", 2);
-		return ;
-	}
-	str[0] = cmd->str;
-	cmd_pop_front();
-	if (!cmd)
-	{
-		ft_putstr_fd("error: cd: bad arguments\n", 2);
-		return ;
-	}
-	str[1] = cmd->str;
-	cmd_pop_front();
-	if (cmd)
-	{
-		ft_putstr_fd("error: cd: bad arguments\n", 2);
-		return ;
-	}
-	if (chdir(str[1]) < 0)
-	{
-		ft_putstr_fd("error: cd: cannot change directory to ", 2);
-		ft_putstr_fd(str[1], 2);
-		ft_putstr_fd("\n", 2);
-		return ;
+		while (cmd->next) cmd = cmd->next;
+		cmd->next = new;
 	}
 }
 
-void	run_child() // remember to free
+int	fd[2] = {0, 1};
+
+void	cd() // no need to exit, need to free
 {
-	for (int i = 0; i  < 2 ; ++i)
+	int	cnt = 0;
+	t_list	*iter = cmd;
+	while (iter)
+	{
+		++cnt;
+		iter = iter->next;
+	}
+	if (cnt != 2)
+	{
+		cmd_clear();
+		ft_putstr_fd( "error: cd: bad arguments\n", 2);
+		return ;
+	}
+	if (chdir(cmd->next->str) < 0)
+	{
+		ft_putstr_fd("error: cd: cannot change directory to ", 2);
+		ft_putstr_fd(cmd->next->str, 2);
+		ft_putstr_fd("\n", 2);
+	}
+	cmd_clear();
+	return ;
+}
+
+void	run_child() // remember to free and to change the fd
+{
+	if (!cmd) return ;
+	for (int i = 0 ; i < 2 ; ++i)
 	{
 		if (fd[i] != i)
 		{
@@ -112,14 +102,15 @@ void	run_child() // remember to free
 			close(fd[i]);
 		}
 	}
-	int	cnt = 0;
-	t_list	*tmp = cmd;
-	while (tmp)
+	int		cnt = 0;
+	t_list	*iter = cmd;
+	while (iter)
 	{
 		++cnt;
-		tmp = tmp->next;
+		iter = iter->next;
 	}
-	char **args = malloc((1 + cnt) * sizeof(char *));
+	char	**args;
+	args = malloc((1 + cnt) * sizeof(char *));
 	args[cnt] = 0;
 	for (int i = 0 ; i < cnt ; ++i)
 	{
@@ -134,28 +125,28 @@ void	run_child() // remember to free
 	exit(EXIT_FAILURE);
 }
 
-void	run_cmd() // remember to free
+void	run_cmd() // remember to free and the fd (need to close ?)
 {
-	if (strcmp("cd", cmd->str) == 0)
+	if (cmd == NULL) return ;
+	if (strcmp(cmd->str, "cd") == 0)
 	{
 		cd();
 		return ;
 	}
-	if (!cmd) return ;
-	pid_t pid = fork();
+	pid_t	pid = fork();
 	if (pid < 0)
 		exit_fatal();
 	else if (pid) // parent
 	{
-		waitpid(pid, NULL, 0);
 		cmd_clear();
-		if (fd[0] != 0)
-			close(fd[0]);
+		if (fd[0] != 0) close(fd[0]);
 		fd[0] = 0;
-		fd[1] = 1;
+		waitpid(pid, NULL, 0);
 	}
 	else // child
+	{
 		run_child();
+	}
 }
 
 int	main(int ac, char **av, char **_env)
@@ -163,22 +154,21 @@ int	main(int ac, char **av, char **_env)
 	env = _env;
 	for (int i = 1 ; i < ac ; ++i)
 	{
-		if (strcmp("|", av[i]) == 0)
+		if (strcmp(av[i], "|") == 0)
 		{
 			int	pipefd[2];
 			if (pipe(pipefd) < 0)
 				exit_fatal();
 			pid_t	pid = fork();
-			if (pid < 0)
-				exit_fatal();
+			if (pid < 0) exit_fatal();
 			else if (pid) // parent
 			{
 				close(pipefd[1]);
 				cmd_clear();
-				waitpid(pid, NULL, 0);
 				if (fd[0] != 0)
 					close(fd[0]);
 				fd[0] = pipefd[0];
+				waitpid(pid, NULL, 0);
 			}
 			else // child
 			{
@@ -186,15 +176,15 @@ int	main(int ac, char **av, char **_env)
 				fd[1] = pipefd[1];
 				run_child();
 			}
-			
 		}
-		else if (strcmp(";", av[i]) == 0)
+		else if (strcmp(av[i], ";") == 0)
 		{
-			if (cmd)
-				run_cmd();
+			run_cmd();
 		}
 		else
+		{
 			cmd_push_back(av[i]);
+		}
 	}
 	if (cmd)
 		run_cmd();
